eb3cb7537b868069338cc486dedb759c4289f2f5
noopali <k2sevak83@gmail.com>
2025-01-14 21:35:42 +0545

    login added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,14 +5,30 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Controllers\Controller;
-
+use Illuminate\Support\Facades\Hash;
 class AuthController extends Controller
 {
     //
 
 
     function login(Request $request){
-        return response()->json(["result"=>"Login Function"]);
+
+        $user = User::where("email",$request->email)->first();
+        if (!$user) {
+            return response()->json(["result" => "user not found"], 404);
+        }
+    
+        if(!Hash::check($request->password,$user->password)){
+
+            return response()->json(["result"=>"password dont match"],404);
+        }
+        else{
+            $success['token'] = $user->createToken('web-app')->plainTextToken;
+            $user['name'] = $user->name;
+
+            return response()->json(["sucess"=>true,"result"=>$success,"message"=>"login sucessful"],201);
+        }
+     
 
     }
     function signup(Request $request){
@@ -20,9 +36,9 @@ class AuthController extends Controller
         $input = $request->all();
         $input["password"] = bcrypt($input["password"]);
         $user = User::create($input);
-        $sucess["token"] = $user->createToken("web-app")->plainTextToken;
+        $success["token"] = $user->createToken("web-app")->plainTextToken;
          $user["name"] = $user->name;
-        return response()->json(["sucess"=>true,"result"=>$sucess,"message"=>"User created sucessfully"]);
+        return response()->json(["succcess"=>true,"result"=>$success,"message"=>"User created sucessfully"]);
         
         
     }
