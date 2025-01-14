a812b3f0d42ebe408c2db728727692aed193ff43
noopali <k2sevak83@gmail.com>
2025-01-14 21:53:12 +0545

    api token authorization

--- routes/api.php
+++ routes/api.php
@@ -12,21 +12,27 @@ Route::get("/test",function(){
     return(["name"=>"ram","surname"=>"thapa"]);
 });
 
-Route::get("/students",[StudentController::class,"list"]);
-
-Route::post("/students",[StudentController::class,"addStudent"]);
-
-Route::put("/students/update",[StudentController::class,"updateStudent"]);
-
-Route::put("/students/update/{id}",[StudentController::class,"updateStudentById"]);
-
-Route::delete("/students/delete/{id}",[StudentController::class,"deleteStudent"]);
-
-Route::get("/students/{name}",[StudentController::class,"searchStudent"]);
-
-Route::get("/members",[MemberController::class,"index"]);
 
 Route::post("/signup",[AuthController::class,"signup"]);
+
 Route::post("/login",[AuthController::class,"login"]);
+
+Route::group(["middleware"=>"auth:sanctum"],function(){
+    Route::get("/students",[StudentController::class,"list"]);
+
+    Route::post("/students",[StudentController::class,"addStudent"]);
+    
+    Route::put("/students/update",[StudentController::class,"updateStudent"]);
+    
+    Route::put("/students/update/{id}",[StudentController::class,"updateStudentById"]);
+    
+    Route::delete("/students/delete/{id}",[StudentController::class,"deleteStudent"]);
+    
+    Route::get("/students/{name}",[StudentController::class,"searchStudent"]);
+    
+    Route::get("/members",[MemberController::class,"index"]);
+    
+
+});
 
 
